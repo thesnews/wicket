8bb78b875726acac39dd62ccc3d5b7728148b833
Mike Joseph <mike.joseph5@gmail.com>
2013-01-10 04:09:59 +0000

    style cleanup

--- app.js
+++ app.js
@@ -3,35 +3,49 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    cons = require('consolidate'),
+    swig = require('swig'),
+    conf = require('./config'),
+    mongoose = require('mongoose');
 
 var app = express();
 
+app.engine('html', cons.swig);
+swig.init({
+    root: __dirname + '/views',
+    autoescape: false,
+    cache: (function() {
+        if( process.env.NODE_ENV == 'production' ) {
+            return true;
+        }
+        return false;
+    })()
+});
+
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+    app.set('port', conf.port || process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'html');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('your secret here'));
+    app.use(express.session());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.enable('trust proxy');
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+require('./routes/sigmund')(app);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+    console.log("Express server listening on port " + app.get('port'));
 });
